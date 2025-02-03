278b45b29b2a51be1bd50344f0c50011cac9c5d0
ZZH <zzh19526308283@outlook.com>
2025-02-03 15:49:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ window.addEventListener('devicemotion', function (event) {
 });
 
 window.addEventListener('resize', function () {
-    if (window.outerHeight - window.innerHeight > 100 || window.outerWidth - window.innerWidth > 100) {
+    if (window.outerHeight - window.innerHeight > 130 || window.outerWidth - window.innerWidth > 130) {
         fuckyou();
     }
 });
